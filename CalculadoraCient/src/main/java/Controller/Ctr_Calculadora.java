7d9482cd5dfa8e452588db0d0251e9b35858a108
Vinicius <79997448+vinivpest@users.noreply.github.com>
2021-10-17 20:26:15 -0300

    Add files via upload

---
+++ CalculadoraCient/src/main/java/Controller/Ctr_Calculadora.java
@@ -0,0 +1,15 @@
+package Controller;
+import Model.*;
+import javax.swing.JOptionPane;
+
+public class Ctr_Calculadora {
+    
+    Calculadora calculadora = new Calculadora();
+   
+    
+    public void SelecionaOperadoracao(Calculadora calculadora){
+        if (calculadora.getOperador() == "+"){
+            calculadora.somar(calculadora.getNumero1(), calculadora.getNumero2());
+        }
+    }
+}
---
+++ CalculadoraCient/src/main/java/Model/Erro.java
@@ -0,0 +1,16 @@
+package Model;
+
+public class Erro{
+    
+    public String Desc;    
+    
+
+    public String getDesc() {
+        return Desc;
+    }
+
+    public void setDesc(String Desc) {
+        this.Desc = Desc;
+    }
+    
+}
